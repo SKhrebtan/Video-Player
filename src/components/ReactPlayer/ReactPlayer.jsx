111b481130387431c5db93b701e697f2d890b531
SKhrebtan <shrebtan@gmail.com>
2023-10-24 15:01:51 +0300

    default url

--- src/components/ReactPlayer/ReactPlayer.jsx
+++ src/components/ReactPlayer/ReactPlayer.jsx
@@ -1,17 +1,18 @@
 import css from './reactplayer.module.css';
 import ReactPlayer from 'react-player';
 
-export const OnlineReactPlayer = ({formValue}) => {
+export const OnlineReactPlayer = ({ formValue }) => {
 
-    return (
-        <div className={css.playerWrapper}>
-        <ReactPlayer
-          className={css.reactPlayer}
-          url={formValue}
-          width='640px'
-          height='480px'
-        />
-        </div>
-    )
-   
+  return (
+    <div className={css.playerWrapper}>
+      <ReactPlayer
+        className={css.reactPlayer}
+        url={formValue ?? 'https://www.youtube.com/watch?v=_fhDVVfELsM&t=1387s'}
+        controls
+        width='640px'
+        height='480px'
+      />
+    </div>
+  )
+
 }
